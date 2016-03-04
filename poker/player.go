d0b0b334b85faedcf1916987769d15b54c15d5e2
Jon Wear <jon@dramafever.com>
2016-03-03 20:46:50 -0500

    adding in comm cards to the pocket cards

--- poker/player.go
+++ poker/player.go
@@ -10,6 +10,11 @@ type Player struct {
 
 func (p *Player) BestHand(cc []Card) Hand {
 	h := Hand{}
-
+	//Right now, just test a flop, 3 community cards
+	p.Hand.Cards[0] = p.Pocket[0]
+	p.Hand.Cards[1] = p.Pocket[1]
+	p.Hand.Cards[2] = cc[0]
+	p.Hand.Cards[3] = cc[1]
+	p.Hand.Cards[4] = cc[2]
 	return h
 }
