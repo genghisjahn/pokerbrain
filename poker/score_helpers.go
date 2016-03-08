7eb0760a21aef7a0c081933efe723132ed1be5b0
Jon Wear <jon@dramafever.com>
2016-03-07 23:17:57 -0500

    remove code

--- poker/score_helpers.go
+++ poker/score_helpers.go
@@ -194,20 +194,3 @@ func GetCardCombinations(cards []Card) []Hand {
 	}
 	return hands
 }
-
-/*
-void print_combinations(const char *string)
-{
-    int i, j, k;
-    int len = strlen(string);
-
-    for (i = 0; i < len - 2; i++)
-    {
-        for (j = i + 1; j < len - 1; j++)
-        {
-            for (k = j + 1; k < len; k++)
-                printf("%c%c%c\n", string[i], string[j], string[k]);
-        }
-    }
-}
-*/
