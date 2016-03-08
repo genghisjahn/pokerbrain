bbbe21a141f3c654bfc1bb7d8e7b0822705465b3
Jon Wear <jon@dramafever.com>
2016-03-07 22:18:59 -0500

    trying to get things ordered right

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func (c *Combos) Swap(i, j int)      { c.Hands[i], c.Hands[j] = c.Hands[j], c.Ha
 func (c *Combos) Less(i, j int) bool { return c.Hands[i].Score() < c.Hands[j].Score() }
 
 func main() {
+	var response string
 	t := poker.Table{}
 	deck := poker.BuildDeck()
 	deck.Shuffle(5)
@@ -31,9 +32,7 @@ func main() {
 		players[k].Pocket[1] = t.Deck.Deal()
 	}
 	t.Players = append(t.Players, players...)
-	t.Flop()
 
-	fmt.Println("Table Cards:", t.CommunityCards)
 	//fmt.Println("Pocket Cards:", t.Players[0].Pocket)
 
 	// fb := []poker.Card{}
@@ -48,17 +47,26 @@ func main() {
 	// sort.Sort(sort.Reverse(&cbo))
 	// fmt.Println("Best Hand:", cbo.Hands[0], cbo.Hands[0].Name, cbo.Hands[0].Score())
 
-	for k, p := range t.Players {
-		t.Players[k].SetBestHand(t.CommunityCards)
+	for _, p := range t.Players {
+		//t.Players[k].SetBestHand(t.CommunityCards)
 		fmt.Println(p.Name, p.Pocket)
 	}
-	fmt.Println(t.CommunityCards)
+	fmt.Scanln(&response)
+	t.Flop()
+	for k := range t.Players {
+		t.Players[k].SetBestHand(t.CommunityCards)
+	}
+	fmt.Println("----")
+	fmt.Println("Flop:", t.CommunityCards)
 	fmt.Println("----")
 	p := t.SortPlayerHands()
 	for _, i := range p {
 		fmt.Println(i)
 	}
-
-	//t.Turn()
+	fmt.Scanln(&response)
+	t.Turn()
+	fmt.Println("----")
+	fmt.Println("Turn:", t.CommunityCards)
+	fmt.Println("----")
 	//t.River()
 }
