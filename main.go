327730921cf97c07ebcf05cf8fe18e9f2c1f10cb
Jon Wear <jon@dramafever.com>
2016-03-08 00:16:51 -0500

    something went wrong

--- main.go
+++ main.go
@@ -14,66 +14,62 @@ type Combos struct {
 
 func (c *Combos) Len() int           { return len(c.Hands) }
 func (c *Combos) Swap(i, j int)      { c.Hands[i], c.Hands[j] = c.Hands[j], c.Hands[i] }
-func (c *Combos) Less(i, j int) bool { return c.Hands[i].Score() < c.Hands[j].Score() }
+func (c *Combos) Less(i, j int) bool { return c.Hands[i].Score < c.Hands[j].Score }
 
 func main() {
-	for {
 
-		t := poker.Table{}
-		deck := poker.BuildDeck()
-		deck.Shuffle(5)
-		t.Deck = deck
-		players := make([]poker.Player, 0, 10)
+	t := poker.Table{}
+	deck := poker.BuildDeck()
+	deck.Shuffle(5)
+	t.Deck = deck
+	players := make([]poker.Player, 0, 10)
 
-		for k := range players[:10] {
-			newPlayer := poker.Player{}
-			newPlayer.Name = names[k]
-			newPlayer.Position = k + 1
-			newPlayer.Stake = 100.0
-			newPlayer.Pocket[0] = t.Deck.Deal()
-			newPlayer.Pocket[1] = t.Deck.Deal()
-			players = append(players, newPlayer)
-		}
-		t.Players = append(t.Players, players...)
-
-		// for _, p := range t.Players {
-		// 	fmt.Println(p.Name, p.Pocket)
-		// }
-		t.Flop()
-		for k := range t.Players {
-			t.Players[k].SetBestHand(t.CommunityCards)
-		}
-		// fmt.Println("----")
-		// fmt.Println("Flop:", t.CommunityCards)
-		// fmt.Println("----")
-		p := t.SortPlayerHands()
-		// for _, i := range p {
-		// 	fmt.Println(i)
-		// }
-		t.Turn()
-		// fmt.Println("----")
-		// fmt.Println("Turn:", t.CommunityCards)
-		// fmt.Println("----")
-		for k, _ := range t.Players {
-			t.Players[k].SetBestHand(t.CommunityCards)
-		}
-		p = t.SortPlayerHands()
-		// for _, i := range p {
-		// 	fmt.Println(i)
-		// }
-		t.River()
-		fmt.Println("----")
-		fmt.Println("River:", t.CommunityCards)
-		fmt.Println("----")
-		for k, _ := range t.Players {
-			t.Players[k].SetBestHand(t.CommunityCards)
-		}
-		p = t.SortPlayerHands()
-		for _, i := range p {
-			fmt.Println(i)
-		}
-		if p[0].Hand.Name == "Flush" {
-			break
-		}
+	for k := range players[:10] {
+		newPlayer := poker.Player{}
+		newPlayer.Name = names[k]
+		newPlayer.Position = k + 1
+		newPlayer.Stake = 100.0
+		newPlayer.Pocket[0] = t.Deck.Deal()
+		newPlayer.Pocket[1] = t.Deck.Deal()
+		players = append(players, newPlayer)
 	}
+	t.Players = append(t.Players, players...)
+
+	// for _, p := range t.Players {
+	// 	fmt.Println(p.Name, p.Pocket)
+	// }
+	t.Flop()
+	for k := range t.Players {
+		t.Players[k].SetBestHand(t.CommunityCards)
+	}
+	// fmt.Println("----")
+	// fmt.Println("Flop:", t.CommunityCards)
+	// fmt.Println("----")
+	p := t.SortPlayerHands()
+	// for _, i := range p {
+	// 	fmt.Println(i)
+	// }
+	t.Turn()
+	// fmt.Println("----")
+	// fmt.Println("Turn:", t.CommunityCards)
+	// fmt.Println("----")
+	for k, _ := range t.Players {
+		t.Players[k].SetBestHand(t.CommunityCards)
+	}
+	p = t.SortPlayerHands()
+	// for _, i := range p {
+	// 	fmt.Println(i)
+	// }
+	t.River()
+	fmt.Println("----")
+	fmt.Println("River:", t.CommunityCards)
+	fmt.Println("----")
+	for k, _ := range t.Players {
+		t.Players[k].SetBestHand(t.CommunityCards)
+	}
+	p = t.SortPlayerHands()
+	for _, i := range p {
+		fmt.Println(i)
+	}
+
 }
