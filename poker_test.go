add51062567a3e9bcd1993f0e60bfc55863eb237
Jon Wear <jon@dramafever.com>
2016-03-08 17:53:41 -0500

    straight test

--- poker_test.go
+++ poker_test.go
@@ -55,3 +55,19 @@ func TestScoreHandThree(t *testing.T) {
 		t.Errorf("Expected 000000000140000001414140802, got %s\n", h.Score)
 	}
 }
+
+func TestScoreHandStraight(t *testing.T) {
+	h := p.Hand{}
+	h.Cards[0] = p.Card{Name: "2", High: 2, Low: 2, Suit: "♡"}
+	h.Cards[1] = p.Card{Name: "3", High: 3, Low: 3, Suit: "♧"}
+	h.Cards[2] = p.Card{Name: "6", High: 6, Low: 6, Suit: "♤"}
+	h.Cards[3] = p.Card{Name: "4", High: 4, Low: 4, Suit: "♤"}
+	h.Cards[4] = p.Card{Name: "5", High: 5, Low: 5, Suit: "♢"}
+	h.SetScore()
+	if h.Name != "Straight" {
+		t.Errorf("Expected Straight, got %s\n", h.Name)
+	}
+	if h.Score != "000000001000000000605040302" {
+		t.Errorf("Expected 000000001000000000605040302, got %s\n", h.Score)
+	}
+}
