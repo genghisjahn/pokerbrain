52de21eda91854c36cd6038f061b81ee617005a7
Jon Wear <jon@dramafever.com>
2016-03-07 23:14:53 -0500

    hands seem to calculate correctly at each part of the flop...cool

--- main.go
+++ main.go
@@ -17,59 +17,63 @@ func (c *Combos) Swap(i, j int)      { c.Hands[i], c.Hands[j] = c.Hands[j], c.Ha
 func (c *Combos) Less(i, j int) bool { return c.Hands[i].Score() < c.Hands[j].Score() }
 
 func main() {
-	var response string
-	t := poker.Table{}
-	deck := poker.BuildDeck()
-	deck.Shuffle(5)
-	t.Deck = deck
-	players := make([]poker.Player, 10, 10)
+	for {
 
-	for k := range players {
-		players[k].Name = names[k]
-		players[k].Position = k + 1
-		players[k].Stake = 100.0
-		players[k].Pocket[0] = t.Deck.Deal()
-		players[k].Pocket[1] = t.Deck.Deal()
-	}
-	t.Players = append(t.Players, players...)
+		t := poker.Table{}
+		deck := poker.BuildDeck()
+		deck.Shuffle(5)
+		t.Deck = deck
+		players := make([]poker.Player, 0, 10)
 
-	for _, p := range t.Players {
-		fmt.Println(p.Name, p.Pocket)
-	}
-	fmt.Scanln(&response)
-	t.Flop()
-	for k := range t.Players {
-		t.Players[k].SetBestHand(t.CommunityCards)
-	}
-	fmt.Println("----")
-	fmt.Println("Flop:", t.CommunityCards)
-	fmt.Println("----")
-	p := t.SortPlayerHands()
-	for _, i := range p {
-		fmt.Println(i)
-	}
-	fmt.Scanln(&response)
-	t.Turn()
-	fmt.Println("----")
-	fmt.Println("Turn:", t.CommunityCards)
-	fmt.Println("----")
-	for k, _ := range t.Players {
-		t.Players[k].SetBestHand(t.CommunityCards)
-	}
-	p = t.SortPlayerHands()
-	for _, i := range p {
-		fmt.Println(i)
-	}
-	fmt.Scanln(&response)
-	t.River()
-	fmt.Println("----")
-	fmt.Println("River:", t.CommunityCards)
-	fmt.Println("----")
-	for k, _ := range t.Players {
-		t.Players[k].SetBestHand(t.CommunityCards)
-	}
-	p = t.SortPlayerHands()
-	for _, i := range p {
-		fmt.Println(i)
+		for k := range players[:10] {
+			newPlayer := poker.Player{}
+			newPlayer.Name = names[k]
+			newPlayer.Position = k + 1
+			newPlayer.Stake = 100.0
+			newPlayer.Pocket[0] = t.Deck.Deal()
+			newPlayer.Pocket[1] = t.Deck.Deal()
+			players = append(players, newPlayer)
+		}
+		t.Players = append(t.Players, players...)
+
+		// for _, p := range t.Players {
+		// 	fmt.Println(p.Name, p.Pocket)
+		// }
+		t.Flop()
+		for k := range t.Players {
+			t.Players[k].SetBestHand(t.CommunityCards)
+		}
+		// fmt.Println("----")
+		// fmt.Println("Flop:", t.CommunityCards)
+		// fmt.Println("----")
+		p := t.SortPlayerHands()
+		// for _, i := range p {
+		// 	fmt.Println(i)
+		// }
+		t.Turn()
+		// fmt.Println("----")
+		// fmt.Println("Turn:", t.CommunityCards)
+		// fmt.Println("----")
+		for k, _ := range t.Players {
+			t.Players[k].SetBestHand(t.CommunityCards)
+		}
+		p = t.SortPlayerHands()
+		// for _, i := range p {
+		// 	fmt.Println(i)
+		// }
+		t.River()
+		fmt.Println("----")
+		fmt.Println("River:", t.CommunityCards)
+		fmt.Println("----")
+		for k, _ := range t.Players {
+			t.Players[k].SetBestHand(t.CommunityCards)
+		}
+		p = t.SortPlayerHands()
+		for _, i := range p {
+			fmt.Println(i)
+		}
+		if p[0].Hand.Name == "Flush" {
+			break
+		}
 	}
 }
