75265c47b543c4b3a694d85a93abfc772ec6c4e8
Jon Wear <jon@dramafever.com>
2016-03-08 18:09:49 -0500

    getting this api thing started

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/genghisjahn/pokerbrain/demo"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
 
 func main() {
-	demo.Demotable()
+	log.Println("Started")
+	http.HandleFunc("/score", scoreHandler)
+
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func scoreHandler(w http.ResponseWriter, r *http.Request) {
+	// http://localhost:8080/hand?h=h14|s14|s10|c8|d4
+	vals := strings.Split(r.FormValue("h"), "|")
+	for _, v := range vals {
+		fmt.Println(v)
+	}
+	http.Error(w, "Not Implemented.", http.StatusInternalServerError)
 }
