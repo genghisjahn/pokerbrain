a13f9e633cf33957c3cc085364cf113a759c4e47
Jon Wear <jon@dramafever.com>
2016-03-08 17:25:25 -0500

    I think I got it

--- poker/player.go
+++ poker/player.go
@@ -14,9 +14,11 @@ type Combos struct {
 	Hands []Hand
 }
 
-func (c *Combos) Len() int           { return len(c.Hands) }
-func (c *Combos) Swap(i, j int)      { c.Hands[i], c.Hands[j] = c.Hands[j], c.Hands[i] }
-func (c *Combos) Less(i, j int) bool { return c.Hands[i].SetScore() < c.Hands[j].SetScore() }
+func (c *Combos) Len() int      { return len(c.Hands) }
+func (c *Combos) Swap(i, j int) { c.Hands[i], c.Hands[j] = c.Hands[j], c.Hands[i] }
+
+//func (c *Combos) Less(i, j int) bool { return c.Hands[i].SetScore() < c.Hands[j].SetScore() }
+func (c *Combos) Less(i, j int) bool { return c.Hands[i].Score < c.Hands[j].Score }
 
 func (p *Player) SetBestHand(cc []Card) {
 	fb := []Card{}
@@ -27,6 +29,9 @@ func (p *Player) SetBestHand(cc []Card) {
 	fb = append(fb, p.Pocket[1])
 	hands := GetCardCombinations(fb)
 	cbo := Combos{Hands: hands}
+	for k := range cbo.Hands {
+		cbo.Hands[k].SetScore()
+	}
 	sort.Sort(sort.Reverse(&cbo)) //GetCardCombinations(cards []Card)
 	p.Hand = cbo.Hands[0]
 
